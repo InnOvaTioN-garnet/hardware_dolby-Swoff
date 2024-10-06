c92fc653d8d5731f1ef2c209a6e95050411dacd2
New Author Name <gh0strider.2k18.reborn@gmail.com>
2024-10-06 12:01:21 +0530

    dolby: Switch to XiaomiDolby
    Based on hardware/xiaomi
    
    History -
    - Enable TARGET_USES_DOLBY
    - Add intelligent equalizer setting
    - Remove deprecated PlainTooltipBox
    - Introduce graphical equalizer
    - Add launcher icon
    - fixup! Restore all settings upon bootup
    - Override AudioFx
    - Use all shared resources from devicesettings
    - Fix build with kotlinc 1.9.0
    - Restore current profile _after_ resetting profiles
    - Do not set volume leveler amount
    - Restore all settings upon bootup
    - Rewrite in Kotlin
    - Revert "Re-enable speaker virtualization after bootup"
    - Convert to SwitchPreferenceCompat
    - Migrate to CompoundButton.OnCheckedChangeListener
    - Enable use_resource_processor for all sysui deps
    - Introduce Dolby Atmos
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: basamaryan <basam.aryan@gmail.com>

--- dolby.mk
+++ dolby.mk
@@ -41,30 +41,40 @@ PRODUCT_COPY_FILES += \
     hardware/dolby/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     hardware/dolby/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
 
+# Dolby VNDK libs
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
 
+# Overlays
 PRODUCT_PACKAGES += \
     DolbyFrameworksResCommon
 
+# Exclude AudioFX
 TARGET_EXCLUDES_AUDIOFX := true
 
+# Dolby
+TARGET_USES_DOLBY := true
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.audio_fx.current=dolby \
     vendor.audio.dolby.ds2.enabled=true \
     vendor.audio.dolby.ds2.hardbypass=true \
     ro.vendor.audio.dolby.dax.support=true \
     ro.vendor.dolby.dax.version=DAX3_3.6.0.12_r1 \
-   
+ 
+# XiaomiDolby and daxService
 PRODUCT_PACKAGES += \
+    XiaomiDolby \
     daxService \
-   
+
+# Dolby UI permissions
+PRODUCT_COPY_FILES += \
+    hardware/dolby/proprietary/system_ext/etc/permissions/privapp-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-com.dolby.daxservice.xml \
+    hardware/dolby/proprietary/system_ext/etc/sysconfig/config-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config-com.dolby.daxservice.xml \
+
 # proprietary blobs
 PRODUCT_COPY_FILES += \
     hardware/dolby/proprietary/odm/bin/hw/vendor.dolby_v3_6.hardware.dms360@2.0-service:$(TARGET_COPY_OUT_ODM)/bin/hw/vendor.dolby_v3_6.hardware.dms360@2.0-service \
-    hardware/dolby/proprietary/system_ext/etc/permissions/privapp-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-com.dolby.daxservice.xml \
-    hardware/dolby/proprietary/system_ext/etc/sysconfig/config-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config-com.dolby.daxservice.xml \
     hardware/dolby/proprietary/vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.dolby.hardware.dms@2.0-service \
     hardware/dolby/proprietary/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.hardware.dms@2.0-service.rc \
     hardware/dolby/proprietary/vendor/lib/libdapparamstorage.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdapparamstorage.so \
@@ -86,4 +96,4 @@ PRODUCT_COPY_FILES += \
     hardware/dolby/proprietary/vendor/lib64/soundfx/libswgamedap.so:$(TARGET_COPY_OUT_VENDOR)/lib64/soundfx/libswgamedap.so \
     hardware/dolby/proprietary/vendor/lib64/soundfx/libswvqe.so:$(TARGET_COPY_OUT_VENDOR)/lib64/soundfx/libswvqe.so \
     hardware/dolby/proprietary/vendor/lib64/vendor.dolby.hardware.dms@2.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.dolby.hardware.dms@2.0-impl.so \
-    hardware/dolby/proprietary/vendor/lib64/vendor.dolby.hardware.dms@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.dolby.hardware.dms@2.0.so
+    hardware/dolby/proprietary/vendor/lib64/vendor.dolby.hardware.dms@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.dolby.hardware.dms@2.0.so \
