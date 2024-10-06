8cf70cde51fd8edcd353e69989fcee5a3bd44330
New Author Name <gh0strider.2k18.reborn@gmail.com>
2024-10-06 13:11:26 +0530

    dolby: Sync with aospa

--- XiaomiDolby/src/co/aospa/dolby/xiaomi/preference/DolbySettingsFragment.kt
+++ XiaomiDolby/src/co/aospa/dolby/xiaomi/preference/DolbySettingsFragment.kt
@@ -12,14 +12,13 @@ import android.media.AudioDeviceInfo
 import android.media.AudioManager
 import android.os.Bundle
 import android.os.Handler
-import android.widget.CompoundButton
-import android.widget.CompoundButton.OnCheckedChangeListener
+import android.widget.Switch
 import android.widget.Toast
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.Preference.OnPreferenceChangeListener
 import androidx.preference.PreferenceFragment
-import androidx.preference.SwitchPreferenceCompat
+import androidx.preference.SwitchPreference
 import co.aospa.dolby.xiaomi.DolbyConstants
 import co.aospa.dolby.xiaomi.DolbyConstants.Companion.PREF_BASS
 import co.aospa.dolby.xiaomi.DolbyConstants.Companion.PREF_DIALOGUE
@@ -36,9 +35,10 @@ import co.aospa.dolby.xiaomi.DolbyConstants.Companion.dlog
 import co.aospa.dolby.xiaomi.DolbyController
 import co.aospa.dolby.xiaomi.R
 import com.android.settingslib.widget.MainSwitchPreference
+import com.android.settingslib.widget.OnMainSwitchChangeListener
 
 class DolbySettingsFragment : PreferenceFragment(),
-    OnPreferenceChangeListener, CompoundButton.OnCheckedChangeListener {
+    OnPreferenceChangeListener, OnMainSwitchChangeListener {
 
     private val switchBar by lazy {
         findPreference<MainSwitchPreference>(PREF_ENABLE)!!
@@ -59,16 +59,16 @@ class DolbySettingsFragment : PreferenceFragment(),
         findPreference<ListPreference>(PREF_DIALOGUE)!!
     }
     private val bassPref by lazy {
-        findPreference<SwitchPreferenceCompat>(PREF_BASS)!!
+        findPreference<SwitchPreference>(PREF_BASS)!!
     }
     private val hpVirtPref by lazy {
-        findPreference<SwitchPreferenceCompat>(PREF_HP_VIRTUALIZER)!!
+        findPreference<SwitchPreference>(PREF_HP_VIRTUALIZER)!!
     }
     private val spkVirtPref by lazy {
-        findPreference<SwitchPreferenceCompat>(PREF_SPK_VIRTUALIZER)!!
+        findPreference<SwitchPreference>(PREF_SPK_VIRTUALIZER)!!
     }
     private val volumePref by lazy {
-        findPreference<SwitchPreferenceCompat>(PREF_VOLUME)!!
+        findPreference<SwitchPreference>(PREF_VOLUME)!!
     }
     private val resetPref by lazy {
         findPreference<Preference>(PREF_RESET)!!
@@ -141,14 +141,14 @@ class DolbySettingsFragment : PreferenceFragment(),
             true
         }
 
-        audioManager!!.registerAudioDeviceCallback(audioDeviceCallback, handler)
+        audioManager.registerAudioDeviceCallback(audioDeviceCallback, handler)
         updateSpeakerState()
         updateProfileSpecificPrefs()
     }
 
     override fun onDestroyView() {
         dlog(TAG, "onDestroyView")
-        audioManager!!.unregisterAudioDeviceCallback(audioDeviceCallback)
+        audioManager.unregisterAudioDeviceCallback(audioDeviceCallback)
         super.onDestroyView()
     }
 
@@ -200,15 +200,15 @@ class DolbySettingsFragment : PreferenceFragment(),
         return true
     }
 
-    override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
-        dlog(TAG, "onCheckedChanged($isChecked)")
+    override fun onSwitchChanged(switchView: Switch, isChecked: Boolean) {
+        dlog(TAG, "onSwitchChanged($isChecked)")
         dolbyController.dsOn = isChecked
         profilePref.setEnabled(isChecked)
         updateProfileSpecificPrefs()
     }
 
     private fun updateSpeakerState() {
-        val device = audioManager!!.getDevicesForAttributes(ATTRIBUTES_MEDIA)[0]
+        val device = audioManager.getDevicesForAttributes(ATTRIBUTES_MEDIA)[0]
         isOnSpeaker = (device.type == AudioDeviceInfo.TYPE_BUILTIN_SPEAKER)
     }
 
@@ -293,7 +293,7 @@ class DolbySettingsFragment : PreferenceFragment(),
     companion object {
         private const val TAG = "DolbySettingsFragment"
         private val ATTRIBUTES_MEDIA = AudioAttributes.Builder()
-            .setUsage(AudioAttributes.USAGE_MEDIA)
-            .build()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build()
     }
 }
